72a14f58f574bb44424833184d974c0e04877379
Ben Amsallem <benchuk@gmail.com>
2026-03-03 02:03:07 +0200

    [MavAI-iOS-SPM]: update: rename package and xcframework asset naming

--- Package.swift
+++ Package.swift
@@ -2,22 +2,22 @@
 import PackageDescription
 
 let releaseName = "v1.0.0"
-let checksumMav2Sdk = "de34a09239562264acba9891e0da89333703e66c545f4a541b37176f7164e5b4"
+let checksumMaverickAiSdk = "de34a09239562264acba9891e0da89333703e66c545f4a541b37176f7164e5b4"
 
 let package = Package(
-    name: "Mav2SDK",
+    name: "Maverick-AI-SDK",
     platforms: [
         .iOS(.v15),
         .watchOS(.v8)
     ],
     products: [
-        .library(name: "Mav2SDK", targets: ["sdk"])
+        .library(name: "Maverick-AI-SDK", targets: ["sdk"])
     ],
     targets: [
         .binaryTarget(
             name: "sdk",
-            url: "https://github.com/everysight-maverick/m2-ios-spm/releases/download/\(releaseName)/mav2sdk.xcframework.zip",
-            checksum: "\(checksumMav2Sdk)"
+            url: "https://github.com/everysight-maverick-AI/mav-ai-ios-spm/releases/download/\(releaseName)/maverick-ai-sdk.xcframework.zip",
+            checksum: "\(checksumMaverickAiSdk)"
         )
     ]
 )
