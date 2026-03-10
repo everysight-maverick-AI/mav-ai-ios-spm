8572069dd88390915f2c12e068e7663eb3df34a3
Benchuk <benchuk@gmail.com>
2026-03-10 22:39:17 +0200

    [Maverick AI SDK]: release: update iOS Package.swift v1.1.2

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
 // swift-tools-version:5.5
 import PackageDescription
 
-let releaseName = "v1.0.0"
-let checksumMaverickAiSdk = "de34a09239562264acba9891e0da89333703e66c545f4a541b37176f7164e5b4"
+let releaseName = "v1.1.2"
+let checksumMav2Sdk = "07c00deb2d0ac953f15e98936561467ac6497131aaf76ffd05da360b90b3c905"
 
 let package = Package(
-    name: "Maverick-AI-SDK",
+    name: "MaverickAISDK",
     platforms: [
         .iOS(.v15),
         .watchOS(.v8)
     ],
     products: [
-        .library(name: "Maverick-AI-SDK", targets: ["sdk"])
+        .library(name: "MaverickAISDK", targets: ["MaverickAI"])
     ],
     targets: [
         .binaryTarget(
-            name: "sdk",
+            name: "MaverickAI",
             url: "https://github.com/everysight-maverick-AI/mav-ai-ios-spm/releases/download/\(releaseName)/maverick-ai-sdk.xcframework.zip",
-            checksum: "\(checksumMaverickAiSdk)"
+            checksum: "\(checksumMav2Sdk)"
         )
     ]
 )
