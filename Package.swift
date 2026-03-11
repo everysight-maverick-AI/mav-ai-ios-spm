6d9a76aa36e55352111c66bd5da6387b3751c6ad
Benchuk <benchuk@gmail.com>
2026-03-11 03:21:01 +0200

    [Maverick AI SDK]: release: update iOS Package.swift v1.1.2

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let releaseName = "v1.1.2"
-let checksumMav2Sdk = "454e5bc6ee4ed9d00fef84f715eb9b6aa6b879ee2e2eecd54765b53b3910d114"
+let checksumMav2Sdk = "9a69abffb9c0a06fc1cba81c03ab14904d33a72e28824a9b85ac2092f1a3fe38"
 
 let package = Package(
     name: "MaverickAISDK",
