8b0f781bbf86d626789da151176a6f2123dac12b
Ben Amsallem <benchuk@gmail.com>
2026-03-26 15:13:08 +0200

    [Maverick AI]: rename package and update README; remove deprecated SDK target

--- Package.swift
+++ Package.swift
@@ -5,24 +5,19 @@ let releaseName = "v1.0.7"
 let checksumMav2Sdk = "9a84baa5adadb7f5c21fd04de04de6a273210a1f4a3c30206175e38943d9f9e0"
 
 let package = Package(
-    name: "MaverickAISDK",
+    name: "MaverickAI",
     platforms: [
         .iOS(.v15),
         .watchOS(.v8)
     ],
     products: [
-        .library(name: "MaverickAISDK", targets: ["MaverickAISDK"])
+        .library(name: "MaverickAI", targets: ["MaverickAI"])
     ],
     targets: [
         .binaryTarget(
             name: "MaverickAI",
             url: "https://github.com/everysight-maverick-AI/mav-ai-ios-spm/releases/download/\(releaseName)/maverick-ai-sdk.xcframework.zip",
             checksum: "\(checksumMav2Sdk)"
-        ),
-        .target(
-            name: "MaverickAISDK",
-            dependencies: ["MaverickAI"],
-            path: "Sources/MaverickAISDK"
         )
     ]
 )
